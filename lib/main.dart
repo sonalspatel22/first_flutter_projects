905ced77f92354c8e195890fbe226d168a984401
jaydeep godhani <godhani.jaydip145@gmail.com>
2022-01-25 12:53:17 +0530

    LoginPage Column TextFeild

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_flutter_projects/pages/home_page.dart';
 import 'package:first_flutter_projects/pages/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,9 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.teal,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+        fontFamily: GoogleFonts.lacquer().fontFamily
       ),
       darkTheme: ThemeData(primarySwatch:Colors.teal),
       // home: HomePage(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,18 +1,75 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text("Login Page",style: TextStyle(
-          fontSize: 20,
-          color: Colors.amber,
-          fontWeight: FontWeight.bold
-        ),),
-      )
+    return Material(
+      //"set text in center"
+      // child: Center(
+      //   child: Text("Login Page",style: TextStyle(
+      //     fontSize: 20,
+      //     color: Colors.amber,
+      //     fontWeight: FontWeight.bold
+      //   ),),
+      // )
+
+      //"set image on whole page"
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/undraw_unlock_24mb.png",
+            height: 200,
+            fit: BoxFit.fitWidth,
+          ),
+          const SizedBox(
+            height: 50.0,
+          ),
+
+          const Text(
+            "Login Page",
+            style: TextStyle(
+                fontSize: 20, color: Colors.amber, fontWeight: FontWeight.bold),
+          ),
+          //
+          const SizedBox(
+            height: 50.0,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Center(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "UserName",
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter PassWord",
+                      labelText: "Password",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: onPressed(),
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
+  onPressed() {
+    print("Hi Login to Flutter");
+  }
 }
